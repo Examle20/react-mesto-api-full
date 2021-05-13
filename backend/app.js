bd6d1d4b7dcf7dea370c3f6cb725ba827903c079
engineer <Examle20@yandex.ru>
2021-05-13 19:38:29 +0300

    feat: связать регистрацию с бэком

--- backend/app.js
+++ backend/app.js
@@ -11,14 +11,10 @@ const {
 const auth = require('./middlewares/auth');
 
 const { PORT = 3001 } = process.env;
-const app = express();
 
+const app = express();
 app.use(cors());
-app.all('*', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  next();
-});
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
