b2e32e5cbe9324dde8ca9868baa77c2cf0962ce7
engineer <Examle20@yandex.ru>
2021-05-13 21:28:00 +0300

    feat: добавить токен к api

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -12,7 +12,7 @@ export class Api {
       {
         headers: {
           authorization: 'Bearer ' + token,
-          'Content-Type': this._contentType,
+          'Content-Type': 'application/json',
         }
       }
     )
@@ -30,7 +30,7 @@ export class Api {
       {
         headers:{
           authorization: 'Bearer ' + token,
-          'Content-Type': this._contentType,
+          'Content-Type': 'application/json',
         }
       }
     )
@@ -42,126 +42,126 @@ export class Api {
         }
       })
   }
-
-  editUserInfo(name, about) {
-    return fetch(`${this._baseUrl}/users/me`,{
-      method: 'PATCH',
-      headers: {
-        authorization: this._authorization,
-        'Content-Type': this._contentType,
-      },
-      body: JSON.stringify({
-        name: name,
-        about: about
-      })
-    })
-      .then(res => {
-        if (!res.ok){
-          return Promise.reject(res.status)
-        } else{
-          return res.json();
-        }
-      })
-  }
-
-  addCard(name, link) {
-    return fetch(`${this._baseUrl}/cards`, {
-      method: 'POST',
-      headers: {
-        authorization: this._authorization,
-        'Content-Type': this._contentType,
-      },
-      body: JSON.stringify({
-        name: name,
-        link: link,
-      })
-    })
-      .then(res => {
-        if (!res.ok){
-          return Promise.reject(res.status)
-        } else{
-          return res.json();
-        }
-      })
-  }
-
-  putLike(_id) {
-    return fetch(`${this._baseUrl}/cards/likes/${_id}`, {
-      method: 'PUT',
-      headers: {
-        authorization: this._authorization,
-        'Content-Type': this._contentType,
-      }
-    })
-      .then(res => {
-        if (!res.ok){
-          return Promise.reject(res.status)
-        } else{
-          return res.json();
-        }
-      })
-  }
-
-  removeLike(_id) {
-    return fetch(`${this._baseUrl}/cards/likes/${_id}`, {
-      method: 'DELETE',
-      headers: {
-        authorization: this._authorization,
-        'Content-Type': this._contentType,
-      }
-    })
-      .then(res => {
-        if (!res.ok){
-          return Promise.reject(res.status)
-        } else{
-          return res.json();
-        }
-      })
-  }
-
-  removeCard(_id) {
-    return fetch(`${this._baseUrl}/cards/${_id}`, {
-      method: 'DELETE',
-      headers: {
-        authorization: this._authorization,
-        'Content-Type': this._contentType,
-      },
-    })
-      .then(res => {
-        if (!res.ok){
-          return Promise.reject(res.status)
-        } else{
-          return res.json();
-        }
-      })
-  }
-
-  changeAvatar(avatar) {
-    return fetch(`${this._baseUrl}/users/me/avatar`, {
-      method: 'PATCH',
-      headers: {
-        authorization: this._authorization,
-        'Content-Type': this._contentType,
-      },
-      body: JSON.stringify({
-        avatar: avatar,
-      })
-    })
-      .then(res => {
-        if (!res.ok){
-          return Promise.reject(res.status)
-        } else{
-          return res.json();
-        }
-      })
-  }
-  changeLikeCardStatus(_id, isLiked) {
-    if(isLiked) {
-      return  this.removeLike(_id);
-    }else {
-      return this.putLike(_id);
-    }
-  }
+//
+//   editUserInfo(name, about) {
+//     return fetch(`${this._baseUrl}/users/me`,{
+//       method: 'PATCH',
+//       headers: {
+//         authorization: this._authorization,
+//         'Content-Type': this._contentType,
+//       },
+//       body: JSON.stringify({
+//         name: name,
+//         about: about
+//       })
+//     })
+//       .then(res => {
+//         if (!res.ok){
+//           return Promise.reject(res.status)
+//         } else{
+//           return res.json();
+//         }
+//       })
+//   }
+//
+//   addCard(name, link) {
+//     return fetch(`${this._baseUrl}/cards`, {
+//       method: 'POST',
+//       headers: {
+//         authorization: this._authorization,
+//         'Content-Type': this._contentType,
+//       },
+//       body: JSON.stringify({
+//         name: name,
+//         link: link,
+//       })
+//     })
+//       .then(res => {
+//         if (!res.ok){
+//           return Promise.reject(res.status)
+//         } else{
+//           return res.json();
+//         }
+//       })
+//   }
+//
+//   putLike(_id) {
+//     return fetch(`${this._baseUrl}/cards/likes/${_id}`, {
+//       method: 'PUT',
+//       headers: {
+//         authorization: this._authorization,
+//         'Content-Type': this._contentType,
+//       }
+//     })
+//       .then(res => {
+//         if (!res.ok){
+//           return Promise.reject(res.status)
+//         } else{
+//           return res.json();
+//         }
+//       })
+//   }
+//
+//   removeLike(_id) {
+//     return fetch(`${this._baseUrl}/cards/likes/${_id}`, {
+//       method: 'DELETE',
+//       headers: {
+//         authorization: this._authorization,
+//         'Content-Type': this._contentType,
+//       }
+//     })
+//       .then(res => {
+//         if (!res.ok){
+//           return Promise.reject(res.status)
+//         } else{
+//           return res.json();
+//         }
+//       })
+//   }
+//
+//   removeCard(_id) {
+//     return fetch(`${this._baseUrl}/cards/${_id}`, {
+//       method: 'DELETE',
+//       headers: {
+//         authorization: this._authorization,
+//         'Content-Type': this._contentType,
+//       },
+//     })
+//       .then(res => {
+//         if (!res.ok){
+//           return Promise.reject(res.status)
+//         } else{
+//           return res.json();
+//         }
+//       })
+//   }
+//
+//   changeAvatar(avatar) {
+//     return fetch(`${this._baseUrl}/users/me/avatar`, {
+//       method: 'PATCH',
+//       headers: {
+//         authorization: this._authorization,
+//         'Content-Type': this._contentType,
+//       },
+//       body: JSON.stringify({
+//         avatar: avatar,
+//       })
+//     })
+//       .then(res => {
+//         if (!res.ok){
+//           return Promise.reject(res.status)
+//         } else{
+//           return res.json();
+//         }
+//       })
+//   }
+//   changeLikeCardStatus(_id, isLiked) {
+//     if(isLiked) {
+//       return  this.removeLike(_id);
+//     }else {
+//       return this.putLike(_id);
+//     }
+//   }
 }
 
 
