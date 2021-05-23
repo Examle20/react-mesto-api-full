49e60009f89e43d908c625daa7ef44fa34361f84
engineer <Examle20@yandex.ru>
2021-05-23 22:17:19 +0300

    feat: сохранять токен в куки

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://api.mesto.examle.nomoredomains.club/';
+export const BASE_URL = 'https://api.mesto.examle.nomoredomains.club/';
 
 export const register = (email, password) => {
   return fetch(`${BASE_URL}/signup`, {
